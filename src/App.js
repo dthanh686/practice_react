01f1c44e90bde2566514c345f2b807147bc49870
thanhtd <thanhtd@vnext.vn>
2023-06-20 21:46:45 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import { Container } from "react-bootstrap";
 import "./App.scss";
 import Header from "./components/Header";
 import TableUsers from "./components/TableUsers";
+import ModalAddUser from "./components/ModalAddUser";
+import { useState } from "react";
 
 const App = () => {
+  const [isShowModal, setIsShowModal] = useState(false);
+  const handleCloseModal = () => {
+    setIsShowModal(false);
+  };
+  const handleOpenModal = () => {
+    setIsShowModal(true);
+  };
+
   return (
     <div className="app-container">
       <Header />
       <Container>
+        <div className="my-3 d-flex align-items-center justify-content-between ">
+          <span>
+            <b>List Users:</b>
+          </span>
+          <button onClick={handleOpenModal} className="btn btn-primary">
+            Add New Users
+          </button>
+        </div>
         <TableUsers />
       </Container>
+      <ModalAddUser show={isShowModal} handleClose={handleCloseModal} />
     </div>
   );
 };
---
+++ src/components/ModalAddUser.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+
+const ModalAddUser = (props) => {
+  const { handleClose, show } = props;
+  const [name, setName] = useState("");
+  const [job, setJob] = useState("");
+
+  const handleSaveUser = () => {};
+
+  return (
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add New User</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Job</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Job"
+                value={job}
+                onChange={(e) => setJob(e.target.value)}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSaveUser}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalAddUser;
